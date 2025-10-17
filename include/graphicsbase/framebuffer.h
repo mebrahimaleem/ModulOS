28205e306d6a1b7be681e81366ee5f3700bfa86c
Ebrahim Aleem <mebrahimaleem@gmail.com>
2025-10-17 14:25:26 -0700

    implemented kernel init from multiboot2

---
+++ include/graphicsbase/framebuffer.h
@@ -0,0 +1,39 @@
+/* framebuffer.h - framebuffer interface */
+/* Copyright (C) 2025  Ebrahim Aleem
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#ifndef GRAPHICSBASE_FRAMEBUFFER_H
+#define GRAPHICSBASE_FRAMEBUFFER_H
+
+#include <stdint.h>
+
+struct framebuffer_t {
+	uint64_t addr;
+	uint32_t height;
+	uint32_t width;
+	uint32_t pitch;
+	enum {
+		VIDEO_NONE,
+		VIDEO_RGB_XRGB8888
+	} mode;
+};
+
+extern void framebuffer_fillrect(
+		struct framebuffer_t* framebuffer, uint32_t startx, uint32_t starty, uint32_t width,
+		uint32_t height, uint8_t r, uint8_t g, uint8_t b);
+
+#endif /* GRAPHICSBASE_FRAMEBUFFER_H */
+
---
+++ include/multiboot2/init.h
@@ -0,0 +1,35 @@
+/* init.h - multiboot2 kernel init interface */
+/* Copyright (C) 2025  Ebrahim Aleem
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#ifndef MULTIBOOT2_INIT_H
+#define MULTIBOOT2_INIT_H
+
+#define MBITAG_TYPE_MEMMAP	6
+#ifdef GRAPHICSBASE
+#define MBITAG_TYPE_FRMBUF	8
+#endif /* GRAPHICSBASE */
+#define MBITAG_TYPE_RSDPV2	15
+
+#ifndef _ASM
+
+struct mb2_info_t;
+
+extern void multiboot2_init(volatile struct mb2_info_t* info);
+
+#endif /* _ASM */
+
+#endif /* MULTIBOOT2_INIT_H */
